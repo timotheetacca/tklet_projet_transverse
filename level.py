d7fea7c7654a1fa9b954faa4db60f19ed142c62e
Blemkeid <busithomas.loesch9+123@gmail.com>
2024-03-23 20:15:38 +0100

    I began to do the display_text_scenario function.

--- level.py
+++ level.py
@@ -1,4 +1,5 @@
 import pygame
+from scenario import display_text_scenario
 
 screen_width, screen_height = 1536, 864
 
@@ -7,7 +8,7 @@ def planet(screen, transparent_surface, position, planet_radius, orbit_radius):
     # Create a surface with the desired transparency
     transparent_surface_planet = pygame.Surface((screen_width, screen_height), pygame.SRCALPHA)
     pygame.draw.circle(transparent_surface, (255, 255, 255, 100), (position[0], position[1]), orbit_radius)
-    pygame.draw.circle(transparent_surface, (255, 255, 255, 250), ((position[0], position[1])), planet_radius)
+    pygame.draw.circle(transparent_surface, (255, 255, 255, 250), (position[0], position[1]), planet_radius)
 
 
 def level(level_number, screen, transparent_surface):
@@ -19,13 +20,7 @@ def level(level_number, screen, transparent_surface):
 
         story = """The year is 2378. In a parallel universe called Nebulaëris, the Orion System shines like a beacon in the black immensity. It is an oasis of life in a sea of nebulae and dead stars. You are Kornus, a talented aerospace engineer. Your childhood best friend, Thorne, shares your passion for space exploration. You grew up together, dreaming of traveling to the stars and discovering the secrets of the universe. But one day, your dream turns into a nightmare. Thorne is abducted by aliens from the planet XFE-462, a mysterious planet located in the Orion System. Your world collapses around you. You can't imagine your life without Thorne. You are determined to save him, no matter the cost. Using your engineering skills, you build a revolutionary rocket capable of traveling from planet to planet. It's a crazy bet, a suicide mission, but you're willing to do anything to find your friend. Your journey will take you through the dangers of space.  But you will never give up hope. You know that Thorne is out there somewhere, waiting to be rescued."""
 
-        sentence_story = story.split(". ")
-
-        for i in range(len(sentence_story)):
-            sentence_story[i] += "."
-
-        while scenario:
-            pass
+        display_text_scenario(story)
 
     if level_number == 1:
         planet_radius = 45
--- scenario.py
+++ scenario.py
@@ -1,7 +1,50 @@
 import pygame
+import time
+
+pygame.init()
+
 screen_width, screen_height = 1536, 864
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 path_font = "Assets/Font/pixela-extreme.ttf"
-def display_text_scenario(string):
+font = pygame.font.Font(path_font, 30)
 
-    pass
+
+def display_text_scenario(story):
+    j = 0
+    sentence_story = story.split(". ")
+    lines = []
+
+    for i in range(len(sentence_story)):
+        sentence_story[i] += "."
+
+    while j != len(sentence_story):
+        text = font.render(sentence_story[j], True, (255, 255, 255))
+        text_rect = text.get_rect()
+
+        if text_rect.width > (screen_width / 2):
+
+            line_temp = ""
+            words = sentence_story[j].split()
+
+            for word in words:
+
+                if font.size(line_temp + word)[0] <= screen_width:
+                    line_temp += word
+                else:
+                    lines.append(line_temp)
+                    line_temp = ""
+
+        text_rect.height *= len(lines)
+        text_rect.center = (screen_width / 2, screen_height / 2)
+
+
+        screen.blit(text, text_rect)
+
+        pygame.display.flip()
+
+        j += 1
+
+        time.sleep(1)
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
