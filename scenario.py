8fefd0eadf8ed52ae2ae47542000851fbc560a5a
Blemkeid <busithomas.loesch9+123@gmail.com>
2024-04-03 21:59:06 +0200

    I FIXED A MAJOR BUG. LESSGOOOO

--- scenario.py
+++ scenario.py
@@ -18,10 +18,12 @@ skip_button_rect.y = 50
 cursor_image = pygame.image.load("Assets/Cursor/cursor_still.png")
 cursor = pygame.transform.scale(cursor_image, (32, 32))
 
+
 def draw_cursor():
     mouse_pos = pygame.mouse.get_pos()
     screen.blit(cursor, (mouse_pos[0], mouse_pos[1]))
     pygame.display.update()
+
 
 def darken_screen(transparency_level):
     surface = pygame.Surface(screen.get_size(), pygame.SRCALPHA)
@@ -39,6 +41,8 @@ def display_text_scenario(story):
     rect.center = (screen_width_divided_by_two, screen_height_divided_by_two)
     white = (255, 255, 255)
     transparency = 0
+    go_to_next_message = True
+    displaying_text = False
 
     for i in range(number_of_sentences - 1):
         sentence_story[i] += "."
@@ -46,89 +50,87 @@ def display_text_scenario(story):
     while True:
 
         for event in pygame.event.get():
-            if event.type == pygame.quit():
+            if event.type == pygame.QUIT:
                 return
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and skip_button_rect.collidepoint(event.pos):
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and skip_button_rect.collidepoint(
+                    event.pos):
                 return
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    j += 1  # Move to the next sentence
+                    if not displaying_text:
+                        j += 1  # Move to the next sentence
+                        go_to_next_message = True
 
         screen.blit(skip_button, skip_button_rect)
 
         text = font.render(sentence_story[j], True, white)
         text_rect = text.get_rect()
-        if text_rect.width > screen_width_divided_by_two:
-            lines = []
-            line_temp = ""
-            words = sentence_story[j].split()
 
-            for word in words:
+        if go_to_next_message:
+            displaying_text = True
+            screen.fill((0, 0, 0))
+            if text_rect.width > screen_width_divided_by_two:
+                lines = []
+                line_temp = ""
+                words = sentence_story[j].split()
 
-                if font.size(line_temp + word + " ")[0] <= screen_width_divided_by_two:
-                    line_temp += word + " "
+                for word in words:
 
-                else:
+                    if font.size(line_temp + word + " ")[0] <= screen_width_divided_by_two:
+                        line_temp += word + " "
+
+                    else:
+                        lines.append(line_temp)
+                        line_temp = word + " "
+
+                if line_temp:
                     lines.append(line_temp)
-                    line_temp = word + " "
 
-            if line_temp:
-                lines.append(line_temp)
+                rect.height = 0
+                rect.width = screen_width_divided_by_two
 
-            rect.height = 0
-            rect.width = screen_width_divided_by_two
+                for line in lines:
+                    line_surface = font.render(line, True, white)
+                    rect.height += line_surface.get_rect().height
 
-            for line in lines:
-                line_surface = font.render(line, True, white)
-                rect.height += line_surface.get_rect().height
+                rect.center = (screen_width_divided_by_two, screen_height_divided_by_two)
+                rect.topleft = (screen_width_divided_by_two / 2, (screen_height - rect.height) / 2)
 
-            rect.center = (screen_width_divided_by_two, screen_height_divided_by_two)
-            rect.topleft = (screen_width_divided_by_two / 2, (screen_height - rect.height) / 2)
+                height_line = 0
+                for line in lines:
+                    for i in range(len(line) + 1):
+                        displayed_text = line[:i]
+                        displayed_text_surface = font.render(displayed_text, True, white)
+                        displayed_text_rect = displayed_text_surface.get_rect(
+                            topleft=(rect.topleft[0], rect.topleft[1] + height_line))
+                        screen.blit(displayed_text_surface, displayed_text_rect)
+                        pygame.display.flip()
+                        time.sleep(0.025)
+                    height_line += displayed_text_rect.height
 
-            height_line = 0
-            for line in lines:
-                for i in range(len(line) + 1):
-                    displayed_text = line[:i]
+            else:
+                text_rect.center = (screen_width_divided_by_two, screen_height_divided_by_two)
+                for i in range(len(sentence_story[j]) + 1):
+                    displayed_text = sentence_story[j][:i]
                     displayed_text_surface = font.render(displayed_text, True, white)
-                    displayed_text_rect = displayed_text_surface.get_rect(
-                        topleft=(rect.topleft[0], rect.topleft[1] + height_line))
+                    displayed_text_rect = text_rect
                     screen.blit(displayed_text_surface, displayed_text_rect)
                     pygame.display.flip()
                     time.sleep(0.025)
-                height_line += displayed_text_rect.height
 
+            if j == number_of_sentences:
+                time.sleep(0.5)
+                fading = True
+                while fading:
+                    darken_screen(transparency)
+                    transparency += 1
+                    pygame.display.flip()
+                    if transparency >= 255:
+                        fading = False
+                break
+
+            go_to_next_message = False
         else:
-            text_rect.center = (screen_width_divided_by_two, screen_height_divided_by_two)
-            for i in range(len(sentence_story[j]) + 1):
-                displayed_text = sentence_story[j][:i]
-                displayed_text_surface = font.render(displayed_text, True, white)
-                displayed_text_rect = text_rect
-                screen.blit(displayed_text_surface, displayed_text_rect)
-                pygame.display.flip()
-                time.sleep(0.025)
-
-        fully_displayed = True
-
-        while fully_displayed:
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE and fully_displayed:
-                        j += 1
-                        fully_displayed = False
-                        break
-
-        if j == number_of_sentences:
-            time.sleep(0.5)
-            fading = True
-            while fading:
-                darken_screen(transparency)
-                transparency += 1
-                pygame.display.flip()
-                if transparency >= 255:
-                    fading = False
-            break
-
+            displaying_text = False
         draw_cursor()
-        pygame.display.update()
-        screen.fill((0, 0, 0))
         pygame.display.flip()
