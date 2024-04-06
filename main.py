3555a4c0dee7241fae412e128446a459c020fc9a
Kim Lan TRAN <150260333+Kimi215@users.noreply.github.com>
2024-04-06 18:23:32 +0200

    changed map level (still got some issues)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from trajectory_simulation import TrajectorySimulation
 from orbital_phase import OrbitalPhase
 from slider import Slider
 from save import update_save_information, remove_life
+from level_map import level_selection
 
 pygame.init()
 pygame.mixer.init()
@@ -11,12 +12,10 @@ pygame.mixer.init()
 # Load and save game information
 level_number, lives = update_save_information("game_save.txt")
 
-
 # Set up the window
 screen_width, screen_height = 1536, 864
 screen = pygame.display.set_mode((screen_width, screen_height), pygame.SRCALPHA)
 pygame.display.set_caption("Efreispace")
-
 
 # Set up the cursor
 cursor_image = pygame.image.load("Assets/Cursor/cursor_still.png")
@@ -42,6 +41,37 @@ image_music_button = green_music_button
 coordinate_music_button = (screen_width - size_music_button + 20, screen_height - size_music_button + 20)
 music_button_rect = green_music_button.get_rect(topleft=coordinate_music_button)
 
+# For Level Map
+background_image_level_map = pygame.image.load("Assets/Switch_Level/background.jpg").convert()
+background_level_map = pygame.transform.scale(background_image_level_map, (screen_width // 2, screen_height))
+
+all_planets_data = [{"image_path": "Assets/Switch_Level/planet1.png", "x": 300},
+    {"image_path": "Assets/Switch_Level/planet2.png", "x": 950},
+    {"image_path": "Assets/Switch_Level/planet3.png", "x": 1600},{"image_path": "Assets/Switch_Level/planet4.png", "x": 2250}]
+planets = []
+planet_rects = []
+for data in all_planets_data:
+    planet_image = pygame.image.load(data["image_path"]).convert_alpha()
+    planet = pygame.transform.scale(planet_image, (250, 250))
+    planet_rect = planet.get_rect()
+    planet_rect.x = data["x"]
+    planet_rect.y = screen_height / 2 - 150
+    planets.append(planet)
+    planet_rects.append(planet_rect)
+
+arrow_data = [{"image_path": "Assets/Switch_Level/left_arrow.png", "x": 75},
+    {"image_path": "Assets/Switch_Level/right_arrow.png", "x": screen_width - 150}]
+arrows = []
+arrow_rects = []
+for data in arrow_data:
+    arrow_image = pygame.image.load(data["image_path"]).convert_alpha()
+    arrow = pygame.transform.scale(arrow_image, (100, 80))
+    arrow_rect = arrow.get_rect()
+    arrow_rect.x = data["x"]
+    arrow_rect.y = screen_height / 2 - 60
+    arrows.append(arrow)
+    arrow_rects.append(arrow_rect)
+
 fps = 120  # Set FPS rate for frame rate
 
 # Initialize TrajectorySimulation instance
@@ -64,10 +94,10 @@ radius = 390
 mouse_held = False
 mouse_pressed = False
 shooting_trajectory = False
-stop_level = False
 orbital_game_phase = False
 music_playing = True
 menu = True
+loaded_level=False
 
 # Initialize the slider
 slider = Slider((50, 50), 200, 0, 100, 50)
@@ -134,47 +164,41 @@ while True:
         pygame.draw.rect(screen, (255, 255, 255), button_rect)
 
     else:
-        if orbital_game_phase is False:
-            if level_attempts < 3:
-                angle = 0
+        if not orbital_game_phase and not loaded_level:
+            loaded_level, level_number = level_selection(screen, background_level_map, planets, planet_rects, arrows,arrow_rects)
 
-                # Calculate v continuously while mouse button is pressed
-                if mouse_pressed:
-                    deplacement_x = position_initial_x - mouse_x
-                    deplacement_y = position_initial_y - mouse_y
+        if loaded_level:
+            angle = 0
+            # Calculate v continuously while mouse button is pressed
+            if mouse_pressed:
+                deplacement_x = position_initial_x - mouse_x
+                deplacement_y = position_initial_y - mouse_y
 
-                    # Calculate the vector from the ball to the mouse
-                    vector_mouse = math.sqrt((mouse_x - position_initial_x) ** 2 + (mouse_y - position_initial_y) ** 2)
+                # Calculate the vector from the ball to the mouse
+                vector_mouse = math.sqrt((mouse_x - position_initial_x) ** 2 + (mouse_y - position_initial_y) ** 2)
 
-                    # Calculate the angle between the x-axis
-                    if vector_mouse != 0:
-                        alpha = math.degrees(math.acos(deplacement_x / vector_mouse))
+                # Calculate the angle between the x-axis
+                if vector_mouse != 0:
+                    alpha = math.degrees(math.acos(deplacement_x / vector_mouse))
 
-                    # Get a velocity from the mouse displacement
-                    v = 40 + deplacement_x / 10 - deplacement_y / 10
+                # Get a velocity from the mouse displacement
+                v = 40 + deplacement_x / 10 - deplacement_y / 10
 
-                # Projectile motion loop
-                if shooting_trajectory:
-                    shooting_trajectory, orbital_game_phase, level_attempts = trajectory_simulation.projectile_motion(circle_x, circle_y, g, v, h, alpha, level_number, level_attempts, clock)
-
-                else:
-                    trajectory_simulation.projectile_aim(g, v, h, alpha, time_step, level_number)
+            # Projectile motion loop
+            if shooting_trajectory:
+                shooting_trajectory, orbital_game_phase, loaded_level = trajectory_simulation.projectile_motion(circle_x, circle_y, g, v, h, alpha, level_number, level_attempts, clock)
             else:
-                print("Should go back to planet selection, -1 life for now")
-                # ⚠ Should call back to menu
-                remove_life("game_save.txt")
-                level_attempts = 0
+                trajectory_simulation.projectile_aim(g, v, h, alpha, time_step, level_number)
 
-        else:
-            # Increment angle for rotation
-            angle -= 0.5
+    if orbital_game_phase:
+        # Increment angle for rotation
+        angle -= 0.6
+        # Draw and handle events for the slider
+        slider.draw(screen)
+        slider_value = slider.slider_value  # Get the current value of the slider
 
-            # Draw and handle events for the slider
-            slider.draw(screen)
-            slider_value = slider.slider_value  # Get the current value of the slider
-
-            # Draw the circle with updated angle
-            orbital_game_phase, level_attempts = orbital_phase.draw_circle(radius, angle)
+        # Draw the circle with updated angle
+        orbital_game_phase = orbital_phase.draw_circle(radius, angle)
 
     # Display the music button
     screen.blit(image_music_button, coordinate_music_button)
